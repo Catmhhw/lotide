b2156e77cb2129249d8cf13f907542138d53afd4
Catmhhw <catherine.mhh.wu@gmail.com>
2022-07-20 00:59:08 -0400

    implement assertArrayEqual which will take in two arrays and console.log a message

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+    if (arrayOne.length === arrayTwo.length) {
+        for (let i = 0; i < arrayOne.length; i++) {
+            if (arrayOne[i] !== arrayTwo[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+const assertArraysEqual = (actual, expected) => {
+    if (eqArrays(actual, expected)) {
+        console.log(`✅✅✅ Assertation Passed ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
+    }
+};
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])) // => false
