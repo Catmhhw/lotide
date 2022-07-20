0dc535d4df3ebe36405b0c32dd6a5d89572e9325
Catmhhw <catherine.mhh.wu@gmail.com>
2022-07-20 04:11:58 -0400

    created a function that takes in an array containing elements including nested arrays of elements, and return a flattened version of the array.

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+    if (arrayOne.length === arrayTwo.length) {
+        for (let i = 0; i < arrayOne.length; i++) {
+            if (arrayOne[i] !== arrayTwo[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+const assertArraysEqual = (actual, expected) => {
+    if (eqArrays(actual, expected)) {
+        console.log(`✅✅✅ Assertation Passed ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
+    }
+};
+
+/////////////////
+
+const flatten = (array) => {
+    let newArray = []
+    let stringArray = array.join().split(",")
+    console.log(stringArray)
+    for (let i = 0; i < stringArray.length; i++) {
+        newArray.push(parseInt(stringArray[i]));
+    }
+    return newArray
+}
+
+
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
