d2fbee27aa9dc1d9784391ca17bec940b3e8e331
Catmhhw <catherine.mhh.wu@gmail.com>
2022-07-30 12:40:32 -0400

    changed line 13 so it checks to see if it is false first instead

--- eqArrays.js
+++ eqArrays.js
@@ -10,15 +10,15 @@ const assertEqual = function (actual, expected) {
 //   assertEqual(1, 1);
 
 const eqArrays = (arrayOne, arrayTwo) => {
-    if (arrayOne.length === arrayTwo.length) {
-        for (let i = 0; i < arrayOne.length; i++) {
-            if (arrayOne[i] !== arrayTwo[i]) {
-                return false;
-            }
-        }
-        return true;
+    if (arrayOne.length !== arrayTwo.length) {
+        return false;
     }
-    return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
