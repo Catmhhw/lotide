9da52c9929492d87750ff0122f8bad3378389dd1
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-03 03:20:49 -0400

    a function the continues to collect items from an array until the callback provides a truthy value.

---
+++ takeUntil.js
@@ -0,0 +1,48 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+    if (arrayOne.length === arrayTwo.length) {
+        for (let i = 0; i < arrayOne.length; i++) {
+            if (arrayOne[i] !== arrayTwo[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+const assertArraysEqual = (actual, expected) => {
+    if (eqArrays(actual, expected)) {
+        console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+const takeUntil = function(array, callback) {
+    results = [];
+    for (item of array) {
+        if (callback(item) === true) {
+            break
+        } else {
+            results.push(item)
+        }
+    }
+    return results
+  }
+
+
+  const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ])
+// [ 1, 2, 5, 7, 2 ]
+// --
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
