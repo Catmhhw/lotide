3ffc501379eb226f157764e3cd78ded1791f0b1a
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-02 23:14:57 -0400

    created a map function that will return a new array based on the results of the callback function.

---
+++ map.js
@@ -0,0 +1,38 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+    if (arrayOne.length === arrayTwo.length) {
+        for (let i = 0; i < arrayOne.length; i++) {
+            if (arrayOne[i] !== arrayTwo[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+const assertArraysEqual = (actual, expected) => {
+    if (eqArrays(actual, expected)) {
+        console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+    // console.log("array: ", array);
+    // console.log("callback: ", callback);
+    const results = [];
+    for (let item of array) {
+        results.push(callback(item));
+        // console.log("item BEFORE ", item);
+        // console.log("item AFTER: ", callback(item));
+        // console.log("---");
+    }
+    return results;
+  }
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ])
