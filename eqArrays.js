4083ea5d367f30ca3b254a4192a1fc60cbd03249
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-06 23:20:56 -0400

    refractored the code and cleaned it up

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,3 @@
-const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
-
-//   assertEqual("Lighthouse Labs", "Bootcamp");
-//   assertEqual(1, 1);
-
 const eqArrays = (arrayOne, arrayTwo) => {
     if (arrayOne.length !== arrayTwo.length) {
         return false;
@@ -21,12 +10,4 @@ const eqArrays = (arrayOne, arrayTwo) => {
     return true;
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+module.exports = eqArrays;
--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = (arrayOne, arrayTwo) => {
-    if (arrayOne.length === arrayTwo.length) {
-        for (let i = 0; i < arrayOne.length; i++) {
-            if (arrayOne[i] !== arrayTwo[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
-const assertArraysEqual = (actual, expected) => {
-    if (eqArrays(actual, expected)) {
-        console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
-
-//////////////////////////////////
-
 const middle = (array) => {
     if (array.length <= 2) {
         return []
@@ -31,21 +9,4 @@ const middle = (array) => {
     }
 }
 
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
-
-
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
